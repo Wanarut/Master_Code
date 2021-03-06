2a81ce0dd08818476051a9027b0bffc18d9f267c
Wanarut <wanarut.b@gmail.com>
2021-03-06 19:19:44 +0700

    set minimum

--- apriori.py
+++ apriori.py
@@ -2,6 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from apyori import apriori
+from timeit import default_timer as timer
+
+minimum_support = 0.7
+minimum_confidence = 0.5
+minimum_lift = 3
+minimum_length = 2
 
 data = pd.read_csv('dataset/chess.dat', header=None, sep=" ")
 print(data.head())
@@ -10,13 +16,17 @@ records = []
 for i in range(data.shape[0]):
     records.append([str(data.values[i, j]) for j in range(data.shape[1])])
 
-print(records)
+print('\nThere are', len(records), len(records[0]), 'transections')
+
+start = timer()
 
 association_rules = apriori(
-    records, min_support=0.0045, min_confidence=0.2, min_lift=3, min_length=2)
+    records, min_support=minimum_support, min_confidence=minimum_confidence, min_lift=minimum_lift, min_length=minimum_length)
 association_rules = list(association_rules)
 
-print(len(association_rules))
+used_time = timer()-start
+print('Found', len(association_rules), 'rules')
+print('Use', pd.to_timedelta(used_time, unit='s'), 'second\n')
 
 for item in association_rules:
 
